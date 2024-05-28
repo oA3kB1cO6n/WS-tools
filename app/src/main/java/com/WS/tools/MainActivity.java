8d574cc0cc20e53fc7fe4a6c7f036d213917c9a1
gollllll <0@a>
2024-05-28 19:34:51 +0800

    emmm…

--- app/src/main/java/com/WS/tools/MainActivity.java
+++ app/src/main/java/com/WS/tools/MainActivity.java
@@ -2,24 +2,15 @@ package com.WS.tools;
 
 import android.annotation.SuppressLint;
 import android.app.Activity;
-import android.app.DownloadManager;
-import android.content.Context;
-import android.content.Intent;
-import android.net.Uri;
 import android.os.Bundle;
-import android.os.Environment;
-import android.webkit.DownloadListener;
-import android.webkit.ValueCallback;
-import android.webkit.WebChromeClient;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
-import com.WS.tools.MimeTypes;
 
 public class MainActivity extends Activity {
 
   private WebView mWebView;
-  private ValueCallback<Uri[]> fileUploadCallback;
+  // private ValueCallback<Uri[]> fileUploadCallback;
 
   @Override
   @SuppressLint("SetJavaScriptEnabled")
@@ -30,60 +21,43 @@ public class MainActivity extends Activity {
     WebSettings webSettings = mWebView.getSettings();
     webSettings.setJavaScriptEnabled(true);
     mWebView.setWebViewClient(new WebViewClient());
-    mWebView.setWebChromeClient(new MyWebChromeClient());
-	mWebView.setDownloadListener(new DownloadListener() {
-	  public void onDownloadStart(String url, String userAgent,
-									String contentDisposition, String mimetype,
-									long contentLength) {
-		DownloadManager.Request request = new DownloadManager.Request(Uri.parse(url));
-
-	    String extension = MimeTypes.getSub(mimetype);
-	    String fileName = getContent(getContent(url, "/", true), ".", false) + extension;
-		request.setDescription(fileName);
-		request.setTitle("下载文件…");
-
-		request.setDestinationInExternalPublicDir(Environment.DIRECTORY_DOWNLOADS, fileName);
-
-		DownloadManager downloadManager = (DownloadManager) getSystemService(Context.DOWNLOAD_SERVICE);
-		downloadManager.enqueue(request);
-	  }
-	});
+    // mWebView.setWebChromeClient(new MyWebChromeClient());
     mWebView.loadUrl("file:///android_asset/CPS.html");
   }
 
-  private class MyWebChromeClient extends WebChromeClient {
-    @Override
-    public boolean onShowFileChooser(WebView webView, ValueCallback<Uri[]> filePathCallback, FileChooserParams fileChooserParams) {
-      fileUploadCallback = filePathCallback;
-      Intent intent = fileChooserParams.createIntent();
-      try {
-        startActivityForResult(intent, 1);
-      } catch (Exception e) {
-        fileUploadCallback = null;
-        return false;
-      }
-      return true;
-    }
-  }
-
-  @Override
-  protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-    super.onActivityResult(requestCode, resultCode, data);
-    if (requestCode == 1) {
-      if (fileUploadCallback == null) {
-        return;
-      }
-      Uri[] results = null;
-      if (resultCode == RESULT_OK && data != null) {
-        String dataString = data.getDataString();
-        if (dataString != null) {
-          results = new Uri[]{Uri.parse(dataString)};
-        }
-      }
-      fileUploadCallback.onReceiveValue(results);
-      fileUploadCallback = null;
-    }
-  }
+//  private class MyWebChromeClient extends WebChromeClient {
+//    @Override
+//    public boolean onShowFileChooser(WebView webView, ValueCallback<Uri[]> filePathCallback, FileChooserParams fileChooserParams) {
+//      fileUploadCallback = filePathCallback;
+//      Intent intent = fileChooserParams.createIntent();
+//      try {
+//        startActivityForResult(intent, 1);
+//      } catch (Exception e) {
+//        fileUploadCallback = null;
+//        return false;
+//      }
+//      return true;
+//    }
+//  }
+//
+//  @Override
+//  protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+//    super.onActivityResult(requestCode, resultCode, data);
+//    if (requestCode == 1) {
+//      if (fileUploadCallback == null) {
+//        return;
+//      }
+//      Uri[] results = null;
+//      if (resultCode == RESULT_OK && data != null) {
+//        String dataString = data.getDataString();
+//        if (dataString != null) {
+//          results = new Uri[]{Uri.parse(dataString)};
+//        }
+//      }
+//      fileUploadCallback.onReceiveValue(results);
+//      fileUploadCallback = null;
+//    }
+//  }
 
   @Override
   public void onBackPressed() {
@@ -92,19 +66,5 @@ public class MainActivity extends Activity {
     } else {
       super.onBackPressed();
     }
-  }
-
-  private String getContent(String str, String str2, boolean after) {
-	int lastIndex = str.lastIndexOf(str2);
-
-	if (lastIndex != -1) {
-	  if (after) {
-		return str.substring(lastIndex + 1);
-	  } else {
-		return str.substring(0, lastIndex);
-	  }
-	} else {
-	  return str;
-	}
   }
 }
